5a233076c644675c7178362bec95ab0aba173cb2
Alex Fomushkin <aleksefo@gmail.com>
2017-04-15 16:32:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/segd/itsalwayssunnysomewhere/MainActivity.java
@@ -0,0 +1,15 @@
+
+package com.example.segd.itsalwayssunnysomewhere;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.segd.itsalwayssunnysomewhere.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+    }
+}
