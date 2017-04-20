6871af9a12c4fef45f609b8b645b4478f720de8b
Alex Fomushkin <aleksefo@gmail.com>
2017-04-20 17:50:36 +0300

    Error handling and polish

--- app/src/main/java/com/example/segd/itsalwayssunnysomewhere/MainActivity.java
+++ app/src/main/java/com/example/segd/itsalwayssunnysomewhere/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.example.segd.itsalwayssunnysomewhere.R;
 import com.example.segd.itsalwayssunnysomewhere.data.SunshinePreferences;
@@ -17,6 +19,8 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
 	private TextView mWeatherTextView;
+	private TextView mErrorMessageDisplay;
+	private ProgressBar mLoadingIndicator;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_forecast);
 
 		mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
-
+		/* This TextView is used to display errors and will be hidden if there are no errors */
+		mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+		mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 		loadWeatherData();
 	}
 
@@ -33,13 +39,35 @@ public class MainActivity extends AppCompatActivity {
 	 * background method to get the weather data in the background.
 	 */
 	private void loadWeatherData() {
+		showWeatherDataView();
+
 		String location = SunshinePreferences.getPreferredWeatherLocation(this);
 		new FetchWeatherTask().execute(location);
 	}
 
+	private void showWeatherDataView() {
+		/* First, make sure the error is invisible */
+		mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+		/* Then, make sure the weather data is visible */
+		mWeatherTextView.setVisibility(View.VISIBLE);
+	}
+
+	private void showErrorMessage() {
+		/* First, hide the currently visible data */
+		mWeatherTextView.setVisibility(View.INVISIBLE);
+		/* Then, show the error */
+		mErrorMessageDisplay.setVisibility(View.VISIBLE);
+	}
+
 	public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
-		// Create a class that extends AsyncTask to perform network requests
+		// override the method onPreExecute and show the loading indicator
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mLoadingIndicator.setVisibility(View.VISIBLE);
+		}
+
 		// Override the doInBackground method to perform your network requests
 		@Override
 		protected String[] doInBackground(String... params) {
@@ -70,15 +98,21 @@ public class MainActivity extends AppCompatActivity {
 		// Override the onPostExecute method to display the results of the network request
 		@Override
 		protected void onPostExecute(String[] weatherData) {
+			mLoadingIndicator.setVisibility(View.INVISIBLE);
 			if (weatherData != null) {
+				//If the weather data was not null, make sure the data view is visible
+				showWeatherDataView();
 				 /*
-			     * Iterate through the array and append the Strings to the TextView. The reason why we add
+				 * Iterate through the array and append the Strings to the TextView. The reason why we add
                  * the "\n\n\n" after the String is to give visual separation between each String in the
                  * TextView. Later, we'll learn about a better way to display lists of data.
                  */
 				for (String weatherString : weatherData) {
 					mWeatherTextView.append((weatherString) + "\n\n\n");
 				}
+			} else {
+				//If the weather data was null, show the error message
+				showErrorMessage();
 			}
 		}
 	}
