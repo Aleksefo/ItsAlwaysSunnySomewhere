a6f3e393643fa5d8ee1a3c8b4b52625f06030e3c
Alex Fomushkin <aleksefo@gmail.com>
2017-04-20 17:34:57 +0300

    menu and refresh btn

--- app/src/main/java/com/example/segd/itsalwayssunnysomewhere/MainActivity.java
+++ app/src/main/java/com/example/segd/itsalwayssunnysomewhere/MainActivity.java
@@ -4,6 +4,9 @@ package com.example.segd.itsalwayssunnysomewhere;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import com.example.segd.itsalwayssunnysomewhere.R;
 import com.example.segd.itsalwayssunnysomewhere.data.SunshinePreferences;
@@ -69,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 		protected void onPostExecute(String[] weatherData) {
 			if (weatherData != null) {
 				 /*
-		         * Iterate through the array and append the Strings to the TextView. The reason why we add
+			     * Iterate through the array and append the Strings to the TextView. The reason why we add
                  * the "\n\n\n" after the String is to give visual separation between each String in the
                  * TextView. Later, we'll learn about a better way to display lists of data.
                  */
@@ -78,5 +81,28 @@ public class MainActivity extends AppCompatActivity {
 				}
 			}
 		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+		MenuInflater inflater = getMenuInflater();
+		        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+		inflater.inflate(R.menu.forecast, menu);
+		        /* Return true so that the menu is displayed in the Toolbar */
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		if (id == R.id.action_refresh) {
+			mWeatherTextView.setText("");
+			loadWeatherData();
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
