ee42e9a5b4bfb31d5c8b5f2f0407ea2f2c282def
Alex Fomushkin <aleksefo@gmail.com>
2017-04-28 01:30:14 +0300

    New activity, intent and back func

--- app/src/main/java/com/example/segd/itsalwayssunnysomewhere/MainActivity.java
+++ app/src/main/java/com/example/segd/itsalwayssunnysomewhere/MainActivity.java
@@ -2,6 +2,7 @@
 package com.example.segd.itsalwayssunnysomewhere;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -54,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
 		mRecyclerView.setHasFixedSize(true);
 
 		/*
-         * The ForecastAdapter is responsible for linking our weather data with the Views that
+	     * The ForecastAdapter is responsible for linking our weather data with the Views that
          * will end up displaying our weather data.
          */
 		mForecastAdapter = new ForecastAdapter(this);
@@ -85,8 +86,9 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
 	@Override
 	public void onClick(String weatherForDay) {
 		Context context = this;
-		Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-			.show();
+		Class destinationClass = DetailActivity.class;
+		Intent intentToStartDetailActivity = new Intent(context, destinationClass);
+		startActivity(intentToStartDetailActivity);
 	}
 
 	private void showWeatherDataView() {
