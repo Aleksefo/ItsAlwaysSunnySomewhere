2281d85c84bcf744ba479fdc3f262b1e72a8297e
Alex Fomushkin <aleksefo@gmail.com>
2017-05-09 22:32:26 +0300

    make sure no duplicate dates exist in the database

--- app/src/main/java/com/example/segd/itsalwayssunnysomewhere/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/segd/itsalwayssunnysomewhere/data/WeatherDbHelper.java
@@ -11,23 +11,24 @@ import com.example.segd.itsalwayssunnysomewhere.data.WeatherContract.WeatherEntr
 
 // Extend SQLiteOpenHelper from WeatherDbHelper
 public class WeatherDbHelper extends SQLiteOpenHelper {
-    /*
-     * This is the name of our database. Database names should be descriptive and end with the
-     * .db extension.
-     */
+
+	/*
+	 * This is the name of our database. Database names should be descriptive and end with the
+	 * .db extension.
+	 */
 	public static final String DATABASE_NAME = "weather.db";
-    /*
-     * If you change the database schema, you must increment the database version or the onUpgrade
-     * method will not be called.
-     *
-     * The reason DATABASE_VERSION starts at 3 is because Sunshine has been used in conjunction
-     * with the Android course for a while now. Believe it or not, older versions of Sunshine
-     * still exist out in the wild. If we started this DATABASE_VERSION off at 1, upgrading older
-     * versions of Sunshine could cause everything to break. Although that is certainly a rare
-     * use-case, we wanted to watch out for it and warn you what could happen if you mistakenly
-     * version your databases.
-     */
-	private static final int DATABASE_VERSION = 2;
+	/*
+	 * If you change the database schema, you must increment the database version or the onUpgrade
+	 * method will not be called.
+	 *
+	 * The reason DATABASE_VERSION starts at 3 is because Sunshine has been used in conjunction
+	 * with the Android course for a while now. Believe it or not, older versions of Sunshine
+	 * still exist out in the wild. If we started this DATABASE_VERSION off at 1, upgrading older
+	 * versions of Sunshine could cause everything to break. Although that is certainly a rare
+	 * use-case, we wanted to watch out for it and warn you what could happen if you mistakenly
+	 * version your databases.
+	 */
+	private static final int DATABASE_VERSION = 3;
 
 	//constructor that accepts a context and call through to the superclass constructor
 	public WeatherDbHelper(Context context) {
@@ -35,6 +36,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 	}
 
 // Override onCreate and create the weather table from within it
+
 	/**
 	 * Called when the database is created for the first time. This is where the creation of
 	 * tables and the initial population of the tables should happen.
@@ -43,8 +45,8 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 	 */
 	@Override
 	public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        /*
-         * This String will contain a simple SQL statement that will create a table that will
+	    /*
+	     * This String will contain a simple SQL statement that will create a table that will
          * cache our weather data.
          */
 		final String SQL_CREATE_WEATHER_TABLE =
@@ -56,21 +58,28 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                  * WeatherEntry implements the interface, "BaseColumns", which does have a field
                  * named "_ID". We use that here to designate our table's primary key.
                  */
-				WeatherEntry._ID               + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+				WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
 
-				WeatherEntry.COLUMN_DATE       + " INTEGER NOT NULL, "                 +
+				WeatherEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
 
-				WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, "                 +
+				WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, " +
 
-				WeatherEntry.COLUMN_MIN_TEMP   + " REAL NOT NULL, "                    +
-				WeatherEntry.COLUMN_MAX_TEMP   + " REAL NOT NULL, "                    +
+				WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
+				WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
 
-				WeatherEntry.COLUMN_HUMIDITY   + " REAL NOT NULL, "                    +
-				WeatherEntry.COLUMN_PRESSURE   + " REAL NOT NULL, "                    +
+				WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
+				WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
 
-				WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, "                    +
-				WeatherEntry.COLUMN_DEGREES    + " REAL NOT NULL" + ");";
+				WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
+				WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
 
+				/*
+                 * To ensure this table can only contain one weather entry per date, we declare
+                 * the date column to be unique. We also specify "ON CONFLICT REPLACE". This tells
+                 * SQLite that if we have a weather entry for a certain date and we attempt to
+                 * insert another weather entry with that date, we replace the old weather entry.
+                 */
+				" UNIQUE (" + WeatherEntry.COLUMN_DATE + ") ON CONFLICT REPLACE);";
         /*
          * After we've spelled out our SQLite table creation statement above, we actually execute
          * that SQL with the execSQL method of our SQLite database object.
@@ -87,8 +96,8 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 	 * method should be your top priority before modifying this method.
 	 *
 	 * @param sqLiteDatabase Database that is being upgraded
-	 * @param oldVersion     The old database version
-	 * @param newVersion     The new database version
+	 * @param oldVersion The old database version
+	 * @param newVersion The new database version
 	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
